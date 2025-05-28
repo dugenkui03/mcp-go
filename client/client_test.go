3699756725b444f796868698816d4ac8dbb2e457
杜艮魁 <dugenkui@bytedance.com>
2025-05-28 13:36:06 +0800

    example

---
+++ client/client_test.go
@@ -0,0 +1,53 @@
+package client
+
+import (
+	"context"
+	"github.com/mark3labs/mcp-go/mcp"
+	"testing"
+	"time"
+)
+
+func TestStreamableHTTPClient(t *testing.T) {
+	client, err := NewStreamableHttpClient("http://localhost:8080/mcp")
+	if err != nil {
+		t.Fatalf("create client failed %v", err)
+		return
+	}
+
+	client.OnNotification(func(notification mcp.JSONRPCNotification) {
+		t.Logf("receive notification %v", notification)
+	})
+
+	ctx := context.Background()
+
+	if err := client.Start(ctx); err != nil {
+		t.Fatalf("Failed to start client: %v", err)
+		return
+	}
+
+	// Initialize
+	initRequest := mcp.InitializeRequest{}
+	initRequest.Params.ProtocolVersion = mcp.LATEST_PROTOCOL_VERSION
+	initRequest.Params.ClientInfo = mcp.Implementation{
+		Name:    "test-client",
+		Version: "1.0.0",
+	}
+
+	_, err = client.Initialize(ctx, initRequest)
+	if err != nil {
+		t.Fatalf("Failed to initialize: %v", err)
+	}
+
+	request := mcp.CallToolRequest{}
+	request.Params.Name = "notify"
+	result, err := client.CallTool(ctx, request)
+	if err != nil {
+		t.Fatalf("CallTool failed: %v", err)
+	}
+
+	// do NOT shutdown immediately
+	time.Sleep(time.Second)
+	if len(result.Content) != 1 {
+		t.Errorf("Expected 1 content item, got %d", len(result.Content))
+	}
+}
